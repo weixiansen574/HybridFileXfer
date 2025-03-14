9229854a0a23ea4a19709aedd53d8dd4ef2c246a
weixiansen574 <1695339869@qq.com>
2025-03-14 13:10:42 +0800

    修复部分Windows系统乱码的问题

--- script/start-by-network.bat
+++ script/start-by-network.bat
@@ -3,5 +3,5 @@ chcp 65001
 set /p IPAddress=Please enter the phone's IP address:  
 echo The IP address you entered is: %IPAddress%  
 
-java -jar HybridFileXfer.jar -c %IPAddress%  
+java -jar -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 HybridFileXfer.jar -c %IPAddress%  
 pause
